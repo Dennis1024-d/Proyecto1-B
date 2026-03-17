4dda8453be5d4ebf47f6ccf391e613159fcec615
Dennis1024-d <dennjos1@gmail.com>
2026-03-17 09:00:53 -0600

    agregue algo

--- Proyecto1-B/Proyecto1-B/obj/Release/net6.0/Proyecto1-B.AssemblyInfo.cs
+++ Proyecto1-B/Proyecto1-B/obj/Release/net6.0/Proyecto1-B.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1-B")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbf9548a251b1e315f4c29ac0b3e5667e011333")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1-B")]
